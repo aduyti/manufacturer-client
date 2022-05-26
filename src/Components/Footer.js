61cd8b850423a2a69676b327efd883a8c08c5968
Anirudhya <anirudhyaduyti@gmail.com>
2022-05-26 22:52:39 +0600

    all home content

---
+++ src/Components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className="bg-base-200 text-slate-600 py-4 mt-3 text-center">
+            All copyright &copy;2022 reserved by The Bolts.
+        </footer>
+    );
+};
+
+export default Footer;
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import Banner from '../Components/Banner';
+import BusinessSummary from '../Components/BusinessSummary';
 import ContactUs from '../Components/ContactUs';
+import FindUs from '../Components/FindUs';
+import Footer from '../Components/Footer';
+import Reviews from '../Components/Reviews';
 import ProductsSection from './ProductsSection';
 
 const Home = () => {
     return (
-        <div className="px-8 md:px-20">
-            <Banner />
-            <ProductsSection />
-            <ContactUs />
-        </div>
+        <>
+            <div className="px-8 md:px-20">
+                <Banner />
+                <ProductsSection />
+                <BusinessSummary />
+                <Reviews />
+                <ContactUs />
+                <FindUs />
+            </div>
+            <Footer />
+        </>
     );
 };
 
