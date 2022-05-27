0766d006916af82c31926bd96a536df75beee2ed
Anirudhya <anirudhyaduyti@gmail.com>
2022-05-27 20:15:52 +0600

    map section

--- src/Components/FindUs.js
+++ src/Components/FindUs.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 
 const FindUs = () => {
     return (
-        <div>
-            find
+        <div className="mb-10 mx-16">
+            <h3 className="text-primary pt-4 text-2xl font-bold text-center">Find Us</h3>
+            <div className="w-full h-96 border-2 rounded-xl overflow-hidden">
+                <LoadScript googleMapsApiKey={process.env.REACT_APP_GoogleMap_API_KEY}>
+                    <GoogleMap
+                        mapContainerStyle={{ width: '100%', height: '100%' }}
+                        center={{
+                            lat: 23.7770374,
+                            lng: 90.3967948
+                        }}
+                        zoom={14}
+                    >
+                        <Marker position={{
+                            lat: 23.7770374,
+                            lng: 90.3967948
+                        }}
+                            cursor="The Bolts Manufacturing" />
+                    </GoogleMap>
+
+                </LoadScript>
+            </div>
+
         </div>
     );
 };
